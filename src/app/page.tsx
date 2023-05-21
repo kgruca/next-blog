0c041f58fc8cbb3a47f1d3b3da4f08248771c373
kgruca <kgruca21@gmail.com>
2023-05-20 19:33:08 -0700

    new page added FirstPost

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => (
+  <>
+    <h1>Blog Home</h1>
+  </>
+);
+
+export default Home;
---
+++ src/app/posts/first-post/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const FirstPost = () => <h1>First Post</h1>;
+
+export default FirstPost;
