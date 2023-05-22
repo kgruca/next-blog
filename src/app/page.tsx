531e1210950b6b803b09d5be7ee77bd3a785a407
kgruca <kgruca21@gmail.com>
2023-05-21 23:43:13 -0700

    head and script added to First Post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Link from "next/link";
 
 const Home = () => (
   <>
     <h1>Blog Home</h1>
+    <h2>
+      Read <Link href="/posts/first-post">this page</Link>!
+    </h2>
   </>
 );
 
--- src/app/posts/first-post/page.tsx
+++ src/app/posts/first-post/page.tsx
@@ -1,5 +1,26 @@
+"use client";
 import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
 
-const FirstPost = () => <h1>First Post</h1>;
+const FirstPost = () => (
+  <>
+    <Head>
+      <title>First Post</title>
+    </Head>
+    <Script
+      src="https://connect.facebook.net/en_US/sdk.js"
+      strategy="lazyOnload"
+      onLoad={() =>
+        console.log(`script loaded correctly, window.FB has been populated`)
+      }
+    />
+    <h1>First Post</h1>
+    <h2>
+      <Link href="/">Back to home</Link>
+    </h2>
+  </>
+);
 
 export default FirstPost;
